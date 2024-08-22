38f43278eb81c0d3254dc6a24b66693ea0106589
Anitashah2007 <anitashah20071386@gmail.com>
2024-08-22 16:55:57 +0330

    Add files via upload

---
+++ zibatariiiiin.py
@@ -0,0 +1,253 @@
+import turtle as t
+t.speed(20)
+t.bgcolor("black")
+t.pencolor("blue")
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(100)
+t.lt(23)
+
+
+t.color("purple")
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(150)
+t.lt(23)
+
+
+t.color("blue")
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(200)
+t.lt(23)
+
+
+t.color("purple")
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
+for i in range (8):
+		t.lt(45)
+		t.fd(250)
+t.lt(23)
